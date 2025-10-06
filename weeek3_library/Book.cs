28a925b7db626eb4b8a1cbf770a61d444fd146d1
CarlHamill95 <hamill-c30@ulster.ac.uk>
2025-10-06 14:50:35 +0100

    updated code with access modifiers

--- weeek3_library/Book.cs
+++ weeek3_library/Book.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace weeek3_library
 {
-    internal class Book
+    class Book
     {
+        string Title;
+        string Author;
+        string ISBN;
+
+        public Book(string bookTitle, string bookAuthor, string bookISBN)
+        {
+            Title = bookTitle;
+            Author = bookAuthor;
+            ISBN = bookISBN;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Book title: {Title}");
+            Console.WriteLine($"Book Author: {Author}");
+            Console.WriteLine($"Book ISBN: {ISBN}");
+            Console.WriteLine();
+        }
     }
 }
